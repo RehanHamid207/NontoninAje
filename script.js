9b4504a717e2f0716d8c2fbc89ba7619bdfb611c
RehanHamid207 <rehanjkt07@gmail.com>
2026-02-20 22:36:51 +0700

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-fetch("https://api.tvmaze.com/shows")
-.then(res => res.json())
-.then(data => {
-  let html = "";
-  data.slice(0,20).forEach(film => {
-    html += `
-      <div class="card">
-        <img src="${film.image.medium}">
-        <h3>${film.name}</h3>
-        <p>⭐ ${film.rating.average || "-"}</p>
-      </div>
-    `;
+fetch("https://api-lk21.herokuapp.com/api/v1/latest")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data); // buat lihat struktur di console
+    let html = "";
+
+    data.forEach(film => {
+      html += `
+        <a href="${film.link || '#'}" target="_blank" class="card">
+          <img src="${film.poster || ''}">
+          <h3>${film.title || 'No Title'}</h3>
+        </a>
+      `;
+    });
+
+    document.getElementById("movies").innerHTML = html;
+  })
+  .catch(err => {
+    document.getElementById("movies").innerHTML = "API tidak bisa diakses (CORS / server mati)";
+    console.log(err);
   });
-  document.getElementById("movies").innerHTML = html;
-});
